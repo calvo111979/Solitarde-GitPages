bcae02b3e538cb17601b01fae78a690607a73cf9
davidsn <davidsrnfire@gmail.com>
2018-06-22 12:15:47 -0500

    Mobile ContactUs form fixed

--- src/containers/ContactUs.js
+++ src/containers/ContactUs.js
@@ -20,17 +20,82 @@ import DesktopContainer from './DesktopContainer'
 import Footer from './Footer'
 import { Form } from 'semantic-ui-react'
 
-const ResponsiveContainer = ({ children }) => (
+const MenuAndBody = ({ children }) => (
   <div>
     <DesktopContainer>{children}</DesktopContainer>
   </div>
 )
 
-ResponsiveContainer.propTypes = {
+MenuAndBody.propTypes = {
   children: PropTypes.node,
 }
 
-class FormExampleSubcomponentControl extends Component {
+
+class MobileForm extends Component {
+  state = {}
+
+  render() {
+    const { children } = this.props
+
+    return (
+      <Responsive {...Responsive.onlyMobile}>
+        <MenuAndBody>
+          <Segment style={{ padding: '3em 0em' }} vertical>
+            <Container text textAlign='center'>
+              <Header as='h1' style={{ fontSize: '2em' }}>
+                CONTACT US
+              </Header>
+            </Container>
+          </Segment>
+          <Segment style={{ padding: '3em 1em' }} vertical>
+            <Grid>
+              <Grid.Row>
+                <Grid.Column>
+                  <Form action="http://solitrade.com/sendmail.php">
+                    <Form.Group widths='equal'>
+                      <Form.Input fluid label='Name' placeholder='Name' />
+                      <Form.Input fluid label='Email' placeholder='Email' />
+                      <Form.Input fluid label='Phone' placeholder='Phone' />
+                    </Form.Group>
+                    <Form.Input fluid label='Subject' placeholder='Subject' />
+                    <Form.TextArea label='About' placeholder='Tell us more' />
+                    <Form.Button>Submit</Form.Button>
+                  </Form>
+                </Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                  <Grid.Column  style={{ padding: '0em 1em' }}>
+                        <Header as='h4' content='CONTACT INFO' />
+                            <Grid.Column style={{ padding: '1em 0em' }}>
+                              <Icon name='phone' color='blue' size='big' />
+                               +1-864-498-8691
+                            </Grid.Column>
+                            <Grid.Column style={{ padding: '1em 0em' }}>
+                              <Icon name='mail' color='blue' size='big'/>
+                               info@solitrade.com
+                            </Grid.Column>
+                            <Grid.Column style={{ padding: '1em 0em' }}>
+                              <Icon name='point' color='blue' size='big'/>
+                               CHARLOTTE, SC 28203 (USA)
+                            </Grid.Column>
+                    </Grid.Column>
+                </Grid.Row>
+              </Grid>
+            </Segment>
+          <Footer />
+        </MenuAndBody>
+      </Responsive>
+    )
+  }
+}
+
+MobileForm.propTypes = {
+  children: PropTypes.node,
+}
+
+
+
+class DesktopForm extends Component {
   state = {}
 
 
@@ -38,51 +103,60 @@ class FormExampleSubcomponentControl extends Component {
   render() {
     const { value } = this.state
     return (
-      <ResponsiveContainer>
-        <Segment style={{ padding: '3em 0em' }} vertical>
-          <Container text textAlign='center'>
-            <Header as='h1' style={{ fontSize: '2em' }}>
-              CONTACT US
-            </Header>
-          </Container>
-        </Segment>
-        <Segment style={{ padding: '3% 25%' }} vertical>
-          <Grid divided='vertically'>
-            <Grid.Row columns={3}>
-              <Grid.Column width={10}>
-                <Form action="http://solitrade.com/sendmail.php">
-                  <Form.Group widths='equal'>
-                    <Form.Input fluid label='Name' placeholder='Name' />
-                    <Form.Input fluid label='Email' placeholder='Email' />
-                    <Form.Input fluid label='Phone' placeholder='Phone' />
-                  </Form.Group>
-                  <Form.Input fluid label='Subject' placeholder='Subject' />
-                  <Form.TextArea label='About' placeholder='Tell us more' />
-                  <Form.Button>Submit</Form.Button>
-                </Form>
-            </Grid.Column>
-            <Grid.Column  style={{ padding: '0em 5%' }} width={6}>
-                <Header as='h4' content='CONTACT INFO' />
-                    <Grid.Column style={{ padding: '1em 0em' }}>
-                      <Icon name='phone' color='blue' size='big' />
-                       +1-864-498-8691
-                    </Grid.Column>
-                    <Grid.Column style={{ padding: '1em 0em' }}>
-                      <Icon name='mail' color='blue' size='big'/>
-                       info@solitrade.com
-                    </Grid.Column>
-                    <Grid.Column style={{ padding: '1em 0em' }}>
-                      <Icon name='point' color='blue' size='big'/>
-                       CHARLOTTE, SC 28203 (USA)
-                    </Grid.Column>
-            </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Segment>
-      <Footer />
-    </ResponsiveContainer>
+      <Responsive {...Responsive.onlyComputer}>
+        <MenuAndBody>
+          <Segment style={{ padding: '3em 0em' }} vertical>
+            <Container text textAlign='center'>
+              <Header as='h1' style={{ fontSize: '2em' }}>
+                CONTACT US
+              </Header>
+            </Container>
+          </Segment>
+          <Segment style={{ padding: '3% 25%' }} vertical>
+            <Grid divided='vertically'>
+              <Grid.Row columns={3}>
+                <Grid.Column width={10}>
+                  <Form action="http://solitrade.com/sendmail.php">
+                    <Form.Group widths='equal'>
+                      <Form.Input fluid label='Name' placeholder='Name' />
+                      <Form.Input fluid label='Email' placeholder='Email' />
+                      <Form.Input fluid label='Phone' placeholder='Phone' />
+                    </Form.Group>
+                    <Form.Input fluid label='Subject' placeholder='Subject' />
+                    <Form.TextArea label='About' placeholder='Tell us more' />
+                    <Form.Button>Submit</Form.Button>
+                  </Form>
+              </Grid.Column>
+              <Grid.Column  style={{ padding: '0em 5%' }} width={6}>
+                  <Header as='h4' content='CONTACT INFO' />
+                      <Grid.Column style={{ padding: '1em 0em' }}>
+                        <Icon name='phone' color='blue' size='big' />
+                         +1-864-498-8691
+                      </Grid.Column>
+                      <Grid.Column style={{ padding: '1em 0em' }}>
+                        <Icon name='mail' color='blue' size='big'/>
+                         info@solitrade.com
+                      </Grid.Column>
+                      <Grid.Column style={{ padding: '1em 0em' }}>
+                        <Icon name='point' color='blue' size='big'/>
+                         CHARLOTTE, SC 28203 (USA)
+                      </Grid.Column>
+              </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Segment>
+        <Footer />
+      </MenuAndBody>
+    </Responsive>
     )
   }
 }
 
-export default FormExampleSubcomponentControl
+const Forms = ({ children }) => (
+  <div>
+    <MobileForm>{children}</MobileForm>
+    <DesktopForm>{children}</DesktopForm>
+  </div>
+)
+
+export default Forms
