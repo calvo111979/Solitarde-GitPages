0eaff41d66677eb0c51395a0131b57320decf8bb
davidsrnfire@gmail.com <davidsrnfire@gmail.com>
2018-06-22 15:46:22 -0500

    Add Tablet Container

--- src/containers/Index.js
+++ src/containers/Index.js
@@ -161,6 +161,79 @@ DesktopContainer.propTypes = {
   children: PropTypes.node,
 }
 
+class TabletContainer extends Component {
+  state = {}
+
+  hideFixedMenu = () => this.setState({ fixed: false })
+  showFixedMenu = () => this.setState({ fixed: true })
+
+  render() {
+    const { children } = this.props
+    const { fixed } = this.state
+
+    return (
+      <Responsive {...Responsive.onlyTablet}>
+        <Favicon url={Favicon} />
+        <Layout title="Solitrade Group"/>
+        <Visibility
+          once={false}
+          onBottomPassed={this.showFixedMenu}
+          onBottomPassedReverse={this.hideFixedMenu}
+        >
+          <Segment
+            textAlign='center'
+            style={{ minHeight: 600, padding: '0em 0em', backgroundImage: `url(${back})`, backgroundSize: 'cover', backgroundPosition: 'center'  }}
+            vertical
+          >
+            <Menu
+              fixed={fixed ? 'top' : null}
+              inverted={!fixed}
+              // pointing={!fixed}
+              // secondary={!fixed}
+              size='large'
+            >
+              <Container style={{  }}>
+                <Menu.Item as='a' href='https://www.linkedin.com/company/solitrade-group'><Link href='https://www.linkedin.com/company/solitrade-group'> <Icon name='linkedin in'/></Link></Menu.Item>
+                {/* <Menu.Item as='a' href='/'><Image src='http://solitrade.com/images/logo-1.png' size='tiny'/></Menu.Item> */}
+                {/* <Menu.Item as='a' href=''><Flag name='es' /></Menu.Item>
+                <Menu.Item as='a' href=''><Flag name='us' /></Menu.Item> */}
+                <Menu.Item as='a' href='/'>HOME</Menu.Item>
+                <Menu.Item as='a' href='/about'>ABOUT US</Menu.Item>
+                <Menu.Item as='a' href='/products'>PRODUCTS</Menu.Item>
+                <Dropdown text='OUR COMMUNITY' pointing className='link item'>
+                  <Dropdown.Menu>
+                    <Dropdown.Item href='/community'>US</Dropdown.Item>
+                    <Dropdown.Item href='/events'>EVENTS</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+                <Menu.Item as='a' href='/contact' >CONTACT US</Menu.Item>
+                {/* <Menu.Item>
+                  <Icon name='phone'/> +1-864-498-8691
+                </Menu.Item>
+                <Menu.Item>
+                  <Icon name='mail'/> info@solitrade.com
+                </Menu.Item> */}
+                <Menu.Item position='right'>
+                  <Button as='a' href="http://sales.solitrade.com">
+                    Log in
+                  </Button>
+                </Menu.Item>
+              </Container>
+            </Menu>
+            <HomepageHeading />
+          </Segment>
+        </Visibility>
+
+        {children}
+      </Responsive>
+    )
+  }
+}
+
+TabletContainer.propTypes = {
+  children: PropTypes.node,
+}
+
 class MobileContainer extends Component {
   state = {}
 
@@ -238,6 +311,7 @@ const ResponsiveContainer = ({ children }) => (
   <div>
     <DesktopContainer>{children}</DesktopContainer>
     <MobileContainer>{children}</MobileContainer>
+    <TabletContainer>{children}</TabletContainer>
   </div>
 )
 
