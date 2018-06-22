91a9f701507c8f22bb16250396a7236409c4f2f0
davidsrnfire@gmail.com <davidsrnfire@gmail.com>
2018-06-22 16:33:27 -0500

    Adding Open Graph

--- static.config.js
+++ static.config.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import React from 'react'
 
 function get_link(lenguage){
   if (lenguage == "en"){
@@ -9,11 +10,32 @@ function get_link(lenguage){
 }
 
 export default {
+  Document: ({ Html, Head, Body, children, siteData, renderMeta }) => (
+    <Html lang="en-US" prefix='og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#'>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta property='og:title' content='Solitrade Group' />
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://solitrade.netlify.com/' />
+        <meta property='og:image' content='https://solitrade.netlify.com/static/logo.e70ee3ee.png' />
+        <meta property='og:description' content='Solitrade Group' />
+        <meta property='og:site_name' content='Solitrade' />
+        <meta property='og:locale' content='es-MX' />
+        <meta property='fb:admins' content='' />
+        <meta property='fb:app_id' content='' />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Body>{children}</Body>
+    </Html>
+  ),
   getSiteData: () => ({
     title: 'Solitrade',
+    lastBuilt: Date.now(),
+    siteRoot: 'https://solitrade.netlify.com/',
+
   }),
   getRoutes: async () => {
-    const { data: events } = await axios.get('http://sales.solitrade.com/json')
+    const { data: events } = await axios.get('https://sales.solitrade.com/json')
     // console.log(events)
     return [
       {
