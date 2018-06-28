244efda51ef5019818ff055311ce8bf9f316ddd8
David Ramirez <davidsrnfire@gmail.com>
2018-06-28 16:33:45 -0500

    Changing everything in the form

--- src/containers/en/ContactUs.js
+++ src/containers/en/ContactUs.js
@@ -1,86 +1,89 @@
-import PropTypes from 'prop-types'
-import React, { Component } from 'react'
-import {
-  Button,
-  Container,
-  Divider,
-  Grid,
-  Header,
-  Icon,
-  Image,
-  List,
-  Menu,
-  Responsive,
-  Segment,
-  Sidebar,
-  Visibility,
-  Flag
-} from 'semantic-ui-react'
-import DesktopContainer from './DesktopContainer'
-import Footer from './Footer'
-import { Form } from 'semantic-ui-react'
-import ClientForm from "./Form"
-const MenuAndBody = ({ children, urlString }) => (
-  <div>
-    <DesktopContainer
-      urlString={urlString}
-      >{children}</DesktopContainer>
-  </div>
-)
+import React from 'react'
+import { Form, Text, TextArea } from 'react-form'
+import axios from 'axios'
 
-MenuAndBody.propTypes = {
-  children: PropTypes.node,
-}
+const encode = data =>
+  Object.keys(data)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+    .join('&')
 
-class DesktopForm extends Component {
-  state = {}
-
-
-
-  render() {
-    const { value } = this.state
+export default class extends React.Component {
+  state = {
+    submitted: false,
+  }
+  render () {
     return (
-      <Responsive>
-        <MenuAndBody urlString="contact">
-          <Container text textAlign='center'>
-            <Header as='h1' style={{ fontSize: '2em', padding:"1em 0em", paddingTop: "2em" }}>
-              CONTACT <span style={{color:"#2185d0"}}>US</span>
-              {/* <Divider style={{color:"#2185d0"}}/> */}
-              <hr style={{
-                width: "10%",
-                border: "1px solid #2185d0",
-              }}/>
-            </Header>
-          </Container>
-          <Segment style={{ padding: '5em 25%', paddingBottom: "5em" }} basic vertical>
-            <Grid divided='vertically'>
-              <Grid.Row columns={3}>
-                <Grid.Column width={10}>
-                  <ClientForm />
-              </Grid.Column>
-              <Grid.Column  style={{ padding: '0em 5%' }} width={6}>
-                  <Header as='h4' content='CONTACT INFO' />
-                      <Grid.Column style={{ padding: '1em 0em' }}>
-                        <Icon name='phone' color='blue' size='big' />
-                         +1-864-498-8691
-                      </Grid.Column>
-                      <Grid.Column style={{ padding: '1em 0em' }}>
-                        <Icon name='mail' color='blue' size='big'/>
-                         info@solitrade.com
-                      </Grid.Column>
-                      <Grid.Column style={{ padding: '1em 0em' }}>
-                        <Icon name='point' color='blue' size='big'/>
-                         CHARLOTTE, SC 28203 (USA)
-                      </Grid.Column>
-              </Grid.Column>
-              </Grid.Row>
-            </Grid>
-          </Segment>
-        <Footer />
-      </MenuAndBody>
-    </Responsive>
+      <div id="contact">
+        <div className="header">Contact Us</div>
+        {this.state.submitted ? (
+          <p className="thanks">
+            Thanks for submitting your information! We'll be in contact with you as soon as
+            possible.
+          </p>
+        ) : (
+          <Form
+            onSubmit={async values => {
+              try {
+                await axios.post('/', encode({ 'form-name': 'contact', ...values }), {
+                  headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+                })
+                this.setState({ submitted: true })
+              } catch (err) {
+                window.alert(
+                  'There was a problem submitting your form! Try again or reload the page :)',
+                )
+                this.setState({ submitted: true })
+              }
+            }}
+          >
+            {({ submitForm }) => (
+              <form name="contact" netlify="true" onSubmit={submitForm}>
+                <div>
+                  <div>Name</div>
+                  <Text field="name" name="name" placeholder="John Doe" />
+                </div>
+                <div>
+                  <div>Email</div>
+                  <Text field="email" name="email" placeholder="johndoe@gmail.com" />
+                </div>
+                <div>
+                  <div>Phone</div>
+                  <Text field="phone" name="phone" placeholder="(555) 555-555" />
+                </div>
+                <div>
+                  <div>Message</div>
+                  <TextArea
+                    field="message"
+                    name="message"
+                    rows="5"
+                    placeholder="Your message to us :)"
+                  />
+                </div>
+                <div>
+                  <button type="submit">Submit</button>
+                </div>
+              </form>
+            )}
+          </Form>
+        )}
+        <div className="columns">
+          <div className="column">
+            Or give us a call for more information about our building and services. We are happy to
+            assist you in planning your upcoming event.
+          </div>
+          <div className="column">
+            <div>City View Receptions</div>
+            <div>1001 East 11th Avenue, Suite B</div>
+            <div>Salt Lake City, UT 84103</div>
+            <a href="tel:+8012901267" className="phone">
+              (801) 290 - 1267
+            </a>
+          </div>
+        </div>
+        <div className="copyright">
+          Copyright &copy; {new Date().getYear() + 1900} City View Receptions
+        </div>
+      </div>
     )
   }
 }
-
-export default DesktopForm
