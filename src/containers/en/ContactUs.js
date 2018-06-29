4ecf6b3d2d5cda3f436a442fcac5c8cd460cb8dc
David Ramirez <davidsrnfire@gmail.com>
2018-06-28 20:37:48 -0500

    maybe? this?!?!?!?

--- src/containers/en/ContactUs.js
+++ src/containers/en/ContactUs.js
@@ -1,117 +1,80 @@
-import React from 'react'
-import { Form, Text, TextArea } from 'react-form'
-import axios from 'axios'
-import {
-  Button,
-  Container,
-  Divider,
-  Grid,
-  Header,
-  Icon,
-  Image,
-  List,
-  Menu,
-  Responsive,
-  Segment,
-  Sidebar,
-  Visibility,
-  Flag
-} from 'semantic-ui-react'
-import DesktopContainer from './DesktopContainer'
-import Footer from './Footer'
+import React from "react";
+// import { navigateTo } from "gatsby-link";
 
-const encode = data =>
-  Object.keys(data)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
-    .join('&')
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+}
 
-export default class extends React.Component {
-  state = {
-    submitted: false,
+export default class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
   }
-  render () {
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...this.state
+      })
+    })
+      // .then(() => navigateTo(form.getAttribute("action")))
+      .catch(error => alert(error));
+  };
+
+  render() {
     return (
-      <DesktopContainer urlString="contact">
-        <Container text textAlign='center'>
-          <Header as='h1' style={{ fontSize: '2em', padding:"1em 0em", paddingTop: "2em" }}>
-            CONTÁCTA<span style={{color:"#2185d0"}}>NOS</span>
-            {/* <Divider style={{color:"#2185d0"}}/> */}
-            <hr style={{
-              width: "10%",
-              border: "1px solid #2185d0",
-            }}/>
-          </Header>
-        </Container>
-        <Container centered>
-          <Grid>
-            <Grid.Row columns={3}>
-              <Grid.Column width={10}>
-                 <div>Contact Us</div>
-                 {this.state.submitted ? (
-                   <p>
-                     Thanks for submitting your information! We'll be in contact with you as soon as
-                     possible.
-                   </p>
-                 ) : (
-                   <Form
-                     onSubmit={async values => {
-                       try {
-                         await axios.post('/', encode({ 'form-name': 'contact', ...values }), {
-                           headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-                         })
-                         this.setState({ submitted: true })
-                       } catch (err) {
-                         window.alert(
-                           'There was a problem submitting your form! Try again or reload the page :)',
-                         )
-                         this.setState({ submitted: true })
-                       }
-                     }}
-                   >
-                     {({ submitForm }) => (
-                       <form name="contact" netlify="true" onSubmit={submitForm}>
-                         <input type="hidden" name="form-name" value="contact" />
-                         <div class="equal width fields">
-                               <div class="field"><label>Name</label>
-                                 <div class="ui fluid input"><input placeholder="Name" name="name" type="text" required/></div>
-                               </div>
-                               <div class="field"><label>Email</label>
-                                 <div class="ui fluid input"><input placeholder="Email" name="email" type="text" required/></div>
-                               </div>
-                               <div class="field"><label>Phone</label>
-                                 <div class="ui fluid input"><input placeholder="Phone" name="phone" type="text" required/></div>
-                               </div>
-                             </div>
-                             <div class="field"><label>Subject</label>
-                               <div class="ui fluid input"><input placeholder="Subject" name="subject" type="text" required/></div>
-                             </div>
-                             <div class="field"><label>About</label><textarea placeholder="Tell us more" name="info" rows="3" required></textarea></div>
-                             <button class="ui button" type="submit">Submit</button>
-                       </form>
-                     )}
-                   </Form>
-                 )}
-               </Grid.Column>
-               <Grid.Column  style={{ padding: '0em 1em' }} width={6}>
-                     <Header as='h4' content='INFO DE CONTACTO' />
-                         <Grid.Column style={{ padding: '1em 0em' }}>
-                           <Icon name='phone' color='blue' size='big' />
-                            +1-864-498-8691
-                         </Grid.Column>
-                         <Grid.Column style={{ padding: '1em 0em' }}>
-                           <Icon name='mail' color='blue' size='big'/>
-                            info@solitrade.com
-                         </Grid.Column>
-                         <Grid.Column style={{ padding: '1em 0em' }}>
-                           <Icon name='point' color='blue' size='big'/>
-                            CHARLOTTE, SC 28203 (USA)
-                         </Grid.Column>
-                 </Grid.Column>
-             </Grid.Row>
-           </Grid>
-         </Container>
-       <Footer />
-     </DesktopContainer>
-    )
+      <div>
+        <h1>Contact</h1>
+        <form
+          name="contact"
+          method="post"
+          action="/success"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={this.handleSubmit}
+        >
+          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+          <input type="hidden" name="form-name" value="contact" />
+          <p hidden>
+            <label>
+              Don’t fill this out:{" "}
+              <input name="bot-field" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your name:<br />
+              <input type="text" name="name" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your email:<br />
+              <input type="email" name="email" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message:<br />
+              <textarea name="message" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+      </div>
+    );
   }
 }
